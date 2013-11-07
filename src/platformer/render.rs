c4bf7eaf6ad0576b432b213c17e49a8b97e23c48
Brendon Justin <bjustin@nerdery.com>
2013-11-07 15:27:29 -0600

    Pass world state into the rendering handler via a port.

--- src/platformer/render.rs
+++ src/platformer/render.rs
@@ -1,7 +1,9 @@
 use std;
 use std::comm::SharedPort;
+use extra::arc::Arc;
+use world;
 
-pub fn rendering_handler(timing_port: SharedPort<()>, render_channel: Chan<int>) {
+pub fn rendering_handler(timing_port: SharedPort<()>, world_state_port: Port<Arc<world::WorldState>>, render_channel: Chan<int>) {
     'render : loop {
         timing_port.recv();
         render_channel.send(0);
