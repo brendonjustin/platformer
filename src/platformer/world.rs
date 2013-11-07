47186dc35f628ed1a744e48572a727539ef29ff6
Brendon Justin <bjustin@nerdery.com>
2013-11-07 11:30:15 -0600

    Send Arc'ed world state from the world state handler.

--- src/platformer/world.rs
+++ src/platformer/world.rs
@@ -1,4 +1,5 @@
 use std::comm::SharedPort;
+use extra::arc::Arc;
 
 struct WorldState {
     ticks: int,
@@ -10,19 +11,20 @@ impl WorldState {
     }
 }
 
-fn update_world_state(world_state: WorldState) -> WorldState {
+fn update_world_state(world_state: &WorldState) -> WorldState {
     WorldState{ticks: world_state.ticks + 1}
 }
 
-pub fn world_handler(timing_port: SharedPort<()>, out_chan: Chan<int>) {
-    let mut world_state = WorldState::new();
+pub fn world_handler(timing_port: SharedPort<()>, out_chan: Chan<Arc<WorldState>>) {
+    let mut world_state = Arc::new(WorldState::new());
 
     'world : loop {
         timing_port.recv();
 
         // update world state
-        world_state = update_world_state(world_state);
+        let updated_state = update_world_state(world_state.get());
+        world_state = Arc::new(updated_state);
 
-        out_chan.send(0);
+        out_chan.send(world_state.clone());
     }
 }
