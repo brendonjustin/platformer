119253213d1d6f68a904e8c14f31e0656a20f3cb
Brendon Justin <brendonjustin@gmail.com>
2013-11-17 16:20:54 -0600

    Avoid hard-coding player sprite path.

--- src/platformer/player.rs
+++ src/platformer/player.rs
@@ -27,18 +27,18 @@ impl Clone for Player {
 
 impl PlayerState {
 	pub fn new(player: ~Player, position: geometry::Point, sprite_path: ~str) -> PlayerState {
-        let sprite = match sdl2::surface::Surface::from_bmp(~Path::new(~"./player.bmp")) {
+        let sprite = match sdl2::surface::Surface::from_bmp(~Path::new(sprite_path.clone())) {
         	Ok(loaded_sprite) => loaded_sprite,
         	Err(_) => fail!(format!("Failed to load player sprite at path: {}", sprite_path))
         };
 
-		PlayerState{player: player, position: position, sprite: sprite, sprite_path: sprite_path}
+		PlayerState{player: player, position: position, sprite: sprite, sprite_path: sprite_path.clone()}
 	}
 }
 
 impl Clone for PlayerState {
 	fn clone(&self) -> PlayerState {
-        let sprite = sdl2::surface::Surface::from_bmp(~Path::new(~"./player.bmp")).unwrap();
+        let sprite = sdl2::surface::Surface::from_bmp(~Path::new(self.sprite_path.clone())).unwrap();
 		PlayerState{player: self.player.clone(), position: self.position, sprite: sprite, sprite_path: self.sprite_path.clone()}
 	}
 }
